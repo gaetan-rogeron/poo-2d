c7ff85177ce6451eb6f4ae029a0f2dbc3eacfea2
gaetan-rogeron <gaetan.dev@pm.me>
2025-11-27 18:44:57 +0100

    animals animation

--- assets/maps/Cow.tsx
+++ assets/maps/Cow.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Cow" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="Cow.png" width="64" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="260"/>
+  </animation>
+ </tile>
 </tileset>
--- assets/maps/Sheep.tsx
+++ assets/maps/Sheep.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Sheep" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="Sheep.png" width="64" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="260"/>
+   <frame tileid="1" duration="260"/>
+   <frame tileid="2" duration="260"/>
+   <frame tileid="3" duration="260"/>
+  </animation>
+ </tile>
 </tileset>
