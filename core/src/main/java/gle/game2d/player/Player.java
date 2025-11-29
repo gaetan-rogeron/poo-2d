44ea5727bdb215ad223d13cff21cc195b683ef86
Lucas <lucasbertin2007@gmail.com>
2025-11-29 01:03:09 +0100

    Changement estethique du joueur

--- core/src/main/java/gle/game2d/player/Player.java
+++ core/src/main/java/gle/game2d/player/Player.java
@@ -10,8 +10,8 @@ import com.badlogic.gdx.math.Vector2;
 /** Représente le personnage joueur dans le jeu. Utilise le patron Strategy pour les comportements et Composite pour les composants. */
 public class Player {
     //Constantes
-    private static final int PLAYER_WIDTH = 16;
-    private static final int PLAYER_HEIGHT = 16;
+    private static final int PLAYER_WIDTH = 32;
+    private static final int PLAYER_HEIGHT = 32;
     private static final float PLAYER_SPEED = 100f;
 
     //Composants (Composition)
